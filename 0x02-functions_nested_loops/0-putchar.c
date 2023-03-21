4c45dfbb30226f211ed74bcca72072cbe7d558bb
TerryMungai <terymungai@gmail.com>
2023-03-21 14:35:56 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include
+main - Entry point
+Return: Always
+int main(vold)
+{ putcharl
+putcharl
+_putcharl
+putcharl
+_putcharl
+putcharl
+putcharl
+putcharl
+_putchar("In'):
+return (0):
+	}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+int _putchar(char c);
+print_alphabet(void):
+void print_alphabet_x18(void);
+int_is lower(int c)i
+int _isalphalint c):
+int print_sign(int n):
+int_abs(int):
+it print_last_digit(int):
+void jack _bauer(void):
+M
+void times table(void):
+int add(int, int):
+void print_to_98(int n):
+void print_tines_table(int n):	
